67a3634ca2779ba30d04bf97ba3761d9f6f606be
宋涛 <songtao@sinosoft.com.cn>
2020-04-29 09:37:38 +0800

    .

---
+++ src/main/java/com/st/mybatis/generator/utils/FileUtil.java
@@ -0,0 +1,11 @@
+package com.st.mybatis.generator.utils;
+
+/**
+ * @ClassName FileUtil
+ * @Description 文件创建、写入工具
+ * @Author songtao
+ * @Date 2020/4/28 0028 18:24
+ **/
+public class FileUtil {
+    public static
+}
